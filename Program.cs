f579ca2b7316c013e9d86c4e7e62f05e679718fa
carter-oleary <carter.m.oleary@outlook.com>
2025-04-10 11:12:28 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Point P1 = new Point(0, 0);
+Point P2 = new Point(-2, 3);
+
+if (P1.getX() < P2.getX())
+{
+    Console.WriteLine("P2 is to the right of P1");
+}
+else if (P1.getX() == P2.getX())
+    Console.WriteLine("P2 is aligned with P1");
+else 
+    Console.WriteLine("P2 is to the left of P1");
